06b4d55e19a79cd3d71c9d07f135c6df688aff2c
EmersonHill <emerson.hill@gmail.com>
2022-04-08 14:02:04 -0400

    Add files via upload
    Init project.

---
+++ src/index.js
@@ -0,0 +1,7 @@
+const React = require('react');
+const ReactDom = require('react-dom');
+
+ReactDOM.render(
+    React.createElement('h2',null,'Hello React'),
+    document.getElementById("root")
+);
